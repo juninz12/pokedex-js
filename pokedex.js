c978d7a2118de06263eb6d8995645fbecd2b43a8
Júnior <hudsonsilva211208@gmail.com>
2026-06-21 15:33:13 -0300

    Primeira versão da Dex de Kanto

---
+++ pokedex.js
@@ -0,0 +1,19 @@
+var npoke = "2"
+npoke = Number.parseInt(npoke)
+
+switch (npoke) {
+    case 1:
+        console.log("Bulbasaur")
+        break
+    case 2: 
+    console.log("ivysaur")
+    break
+    case 3:
+        console.log("venossaur")
+        break
+        case 4:
+            console.log("Charmander")
+            break
+            default:
+                console.log("Pokemon não encontrado")
+}
